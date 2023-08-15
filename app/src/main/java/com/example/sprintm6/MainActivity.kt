1ab1c6e6529cc27a83a157dc8eef40bb358e6cb5
ginger cea <gingercea@gmail.com>
2023-08-14 22:23:13 -0400

    Inicio del Proyecto se importo r y ademas se agrega los permisos en manifest

--- app/src/main/java/com/example/sprintm6/MainActivity.kt
+++ app/src/main/java/com/example/sprintm6/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sprintm6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ej5m6.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
